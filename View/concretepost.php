7500698fe6c0c02ba5ff2f8dd3b7eb1341db01e1
Paweł Czarny <czarny.pawel@student.pk.edu.pl>
2018-05-17 21:46:10 +0200

    add some views

---
+++ View/concretepost.php
@@ -0,0 +1,22 @@
+<?php include_once '../Config/config.php'; 
+/*this fil is for display concrete post*/
+$stmt = $db->prepare(...
+$stmt->execute(..);
+$row = $stmt->fetch();
+                     
+<!DOCTYPE html>
+<?php include_once 'header.php'; ?>
+<html lang="en">
+<body>
+<div class="container">
+	<h1>Programmer-helper</h1>
+		<hr />
+        <p><a href="./">Site Index</a></p>
+		<?php
+
+		?>
+   
+<?php include_once 'footer.php'; ?>
+</div>
+</body>
+</html>
---
+++ View/index.php
@@ -0,0 +1,28 @@
+<?php include_once '../Config/config.php'; ?>
+
+<!DOCTYPE html>
+<?php include_once 'header.php'; ?>
+<html lang="en">
+<body>
+<div class="container">
+	<h1>Programmer-helper</h1>
+		<hr />
+
+		<?php
+			/*try {
+                Show some posts from db
+				$stmt = $db->query('SELECT ...');
+					
+
+
+				
+
+			} catch(PDOException $e) {
+			    echo $e->getMessage();
+			}*/
+		?>
+   
+<?php include_once 'footer.php'; ?>
+</div>
+</body>
+</html>
